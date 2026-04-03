2367c7f46345f5d64e199afb82bf9c0ee44a79de
BhumikaCoder21 <bhumikanew21@gmail.com>
2026-04-03 10:52:07 +0530

    Implemented authentication with JWT and bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const pool = require("./config/db");
@@ -17,7 +18,10 @@ app.get("/", (req, res) => {
   res.send("LetsChat backend running");
 });
 
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
