40e721555e6348719ab9631a383f436af0c32fbf
BhumikaCoder21 <bhumikanew21@gmail.com>
2026-04-03 12:34:00 +0530

    Added real-time messaging using Socket.IO

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -8,8 +8,24 @@ exports.sendMessage = async (req, res) => {
       "INSERT INTO messages (chat_id, sender_id, content) VALUES ($1, $2, $3) RETURNING *",
       [chatId, senderId, content],
     );
+    const message = newMessage.rows[0];
+    const io = req.app.get("io");
 
-    res.json(newMessage.rows[0]);
+    const usersInChat = await pool.query(
+       "SELECT user_id FROM chat_members WHERE chat_id = $1",
+       [chatId],
+     );
+
+     usersInChat.rows.forEach((user) => {
+       const socketId = global.onlineUsers?.[user.user_id];
+       if (socketId) {
+         io.to(socketId).emit("receive_message", message);
+       }
+     });
+
+      
+      res.json(message);
+      
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
+
 const authRoutes = require("./routes/authRoutes");
 const authMiddleware = require("./middleware/authMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
-const app = express();
 const pool = require("./config/db");
+
+const app = express();
+
 
 pool
   .query("SELECT 1")
@@ -17,9 +22,37 @@ pool
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   res.send("LetsChat backend running");
 });
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const users = {};
+global.onlineUsers = users;
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join", (userId) => {
+    users[userId] = socket.id;
+    console.log("User joined:", userId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+global.onlineUsers = users;
+app.set("io", io);
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
@@ -32,8 +65,8 @@ app.get("/protected", authMiddleware, (req, res) => {
   });
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
